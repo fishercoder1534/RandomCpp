52005510d55346bfd421c3bd5e0c1a2b1146ba77
Fisher Coder <fishercoder@gmail.com>
2019-03-24 12:01:13 -0700

    add reference example

--- Foo.cpp
+++ Foo.cpp
@@ -21,4 +21,16 @@ int main(int argc, char** argv) {
     Foo* foo = new Foo();
     foo->myMethod1();
     delete foo;
+
+    //references
+    int foo3 = 10;
+    int& bar = foo3;//bar is a reference of foo3, so changing either bar or foo3, will change the other one as well.
+
+    bar += 20;
+    printf("foo3 is: %d\n", foo3);
+    printf("bar is: %d\n", bar);
+
+    foo3 = 15;
+    printf("foo3 is: %d\n", foo3);
+    printf("bar is: %d\n", bar);
 }
