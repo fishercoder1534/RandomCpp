37e09f96fa4e4c94c29ad667843b0463c8594764
Fisher Coder <fishercoder@gmail.com>
2019-03-24 12:30:45 -0700

    print out more info to help understand:

--- Foo.cpp
+++ Foo.cpp
@@ -42,11 +42,26 @@ int main(int argc, char** argv) {
     Foo* myFooPointer;//declare a pointer to Foo class
     myFooPointer = &myFooInstance;//set the value of the pointer to the address of myFooInstance. "&" means to get the address of a variable
     //this is also the key to remember!!!!
-    // They are two different copies!
     printf("&myFooInstance: %p\n", &myFooInstance);
     printf("&myFooPointer: %p\n", &myFooPointer);
 
+    printf("myFooInstance.m_variable1: %d\n", myFooInstance.m_variable1);
+    printf("myFooPointer->m_variable1 %d\n", myFooPointer->m_variable1);
 
+    // changing either bar or foo3, will change the other one as well.
+    myFooInstance.m_variable1 = 123;
+    printf("after changing myFooInstance.m_variable1 value:\n");
+    printf("myFooInstance.m_variable1: %d\n", myFooInstance.m_variable1);
+    printf("myFooPointer->m_variable1 %d\n", myFooPointer->m_variable1);
+
+    // changing either bar or foo3, will change the other one as well.
+    myFooPointer->m_variable1 = 456;
+    printf("after changing myFooPointer->m_variable1 value:\n");
+    printf("myFooInstance.m_variable1: %d\n", myFooInstance.m_variable1);
+    printf("myFooPointer->m_variable1 %d\n", myFooPointer->m_variable1);
+
+
+    printf("\n\nThe other way: making a pointer from a local variable:\n");
     //the other way: making a pointer from a local variable
     Foo* fooPointer = new Foo();//create a pointer to Foo
     Foo myFooInstance2 = *fooPointer;//"*" means to dereference the pointer and then assign it to myFooInstance2; copy is made!
@@ -54,6 +69,21 @@ int main(int argc, char** argv) {
     //this is key!!!!!!!!! remmeber this! It dereferences the pointer and assigns it to myFooInstance2!!!
     printf("&fooPointer: %p\n", &fooPointer);
     printf("&myFooInstance2: %p\n", &myFooInstance2);
+
+    printf("fooPointer->m_variable2: %d\n", fooPointer->m_variable2);
+    printf("myFooInstance2.m_variable2: %d\n", myFooInstance2.m_variable2);
+
+    // They are two different copies! chaging one will not change the other one!
+    fooPointer->m_variable2 = 987;
+    printf("\n\nafter changing fooPointer->m_variable2 value:\n");
+    printf("fooPointer->m_variable2: %d\n", fooPointer->m_variable2);
+    printf("myFooInstance2.m_variable2: %d\n", myFooInstance2.m_variable2);
+
+    // They are two different copies! chaging one will not change the other one!
+    myFooInstance2.m_variable2 = 654;
+    printf("\n\nafter changing myFooInstance2.m_variable2 value:\n");
+    printf("fooPointer->m_variable2: %d\n", fooPointer->m_variable2);
+    printf("myFooInstance2.m_variable2: %d\n", myFooInstance2.m_variable2);
 
 
     cout << "Program finished!\n";
