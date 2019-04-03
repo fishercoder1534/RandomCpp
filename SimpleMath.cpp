9915b49dca10d94ca4426bb1d3b5240807c5ccb8
Fisher Coder <fishercoder@gmail.com>
2019-04-03 14:20:58 -0700

    add simple math example

---
+++ SimpleMath.cpp
@@ -0,0 +1,10 @@
+// SimpleMath.cpp : Defines the entry point for the console application.
+#include "SimpleMath.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << cubic(10) << endl;
+	return 0;
+}
