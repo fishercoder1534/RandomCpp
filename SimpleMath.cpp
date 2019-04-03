da64b00f34a6d70f71389339cf30c2c6899f977f
Fisher Coder <fishercoder@gmail.com>
2019-04-03 15:27:17 -0700

    refactor SimpleMath*

--- SimpleMath.cpp
+++ SimpleMath.cpp
@@ -1,10 +1,20 @@
 // SimpleMath.cpp : Defines the entry point for the console application.
 #include "SimpleMath.h"
 #include <iostream>
+#include <math.h>
 using namespace std;
+ 
+double squareRoot(const double a) {
+    double b = sqrt(a);
+    if(b != b) { // nan check
+        return -1.0;
+    }else{
+        return sqrt(a);
+    }
+}
 
-int main()
-{
-	cout << cubic(10) << endl;
-	return 0;
+int main(int argc, char** argv) {
+    cout << "program started.\n";
+    cout << squareRoot(16.0) << endl;
+    cout << "program finished.\n";
 }
